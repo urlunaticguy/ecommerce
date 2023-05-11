8ac9cb77ffb3d8822a75bc147c59d43c4098924f
Souvik Das <souvikvillain@icloud.com>
2023-05-11 08:38:00 +0530

    adds ssr support for product card

--- pages/frontend/screens/LandingScreen/comps/ProductsContainer/comps/ProductCard.jsx
+++ pages/frontend/screens/LandingScreen/comps/ProductsContainer/comps/ProductCard.jsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
 import { Rating } from 'react-simple-star-rating'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ProductCard(props) {
     const [rating, setRating] = useState(0) // initial rating value
+
+    const [image, setImage] = useState("/assets/images/sample_image.png")
+    const [name, setName] = useState("Sample Name")
+    const [price, setPrice] = useState("500")
+    const [description, setDescription] = useState("Sample Description")
+
+    useEffect(() => {
+        setImage(props.product.image)
+        setName(props.product.name)
+        setPrice(props.product.price)
+        setDescription(props.product.description)
+    }, [props])
 
   // Catch Rating value
   const handleRating = (rate) => {
@@ -12,13 +24,13 @@ export default function ProductCard(props) {
   }
     return (
         <div className=" w-[16rem] h-[20rem] rounded-xl p-2 flex flex-col border-2 border-slate-300">
-            <Image width={256} height={256} src={props.product.image} />
+            <Image width={256} height={256} src={image} />
             <div className=" flex flex-row justify-between items-center font-semibold">
-                <h1>{props.product.name}</h1>
-                <h1>{props.product.price}</h1>
+                <h1>{name}</h1>
+                <h1>{price}</h1>
             </div>
             <div className=" font-light text-[0.75rem] items-center flex justify-start">
-                <h2>{props.product.description}</h2>
+                <h2>{description}</h2>
             </div>
             <div className=" flex flex-row">
                 <Rating
